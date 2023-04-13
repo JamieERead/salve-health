0424ac5783c6cc060b962e9e234756273e8f0612
Jamie Read <james.read@sideshowagency.com>
2023-04-13 13:10:45 +0100

    feat: :sparkles: added basic express API for serving csv files

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const csv = require('csv-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('.'));
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+const parseCSV = (fileName) => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream(fileName)
+      .on('error', err => reject(err))
+      .pipe(csv())
+      .on('data', data => results.push(data))
+      .on('end', () => resolve(results));
+  });
+}
+
+app.get('/api/getClinics', async (req, res) => {
+  try {
+    const results = await parseCSV('data/clinics.csv');
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error getting clinic data');
+  }
+});
+
+app.get('/api/getPatients/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const results = await parseCSV(`data/patients-${id}.csv`);
+    res.json(results);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error getting patient data');
+  }
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
